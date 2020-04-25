8d0539ff4b2aab236d8415c607cec861836fc13a
trahul2008 <41509095+trahul2008@users.noreply.github.com>
2020-04-25 16:43:36 +0530

    Create calci.py

---
+++ calci.py
@@ -0,0 +1,18 @@
+print("Hello World");
+a=int(input("enter number a:"));
+b=int(input("enter number b:"));
+operator=input("enter * for multiplication, + for addition, - for subtraction and / for division:");
+
+if (operator == '*'):
+    result=a*b;
+    print(result);
+elif (operator == '+'):
+    result=a+b;
+    print(result);
+elif (operator == '-'):
+    result=a-b;
+    print(result);
+elif (operator == '/'):
+    print(result);
+else:
+    print("invalid operator");
